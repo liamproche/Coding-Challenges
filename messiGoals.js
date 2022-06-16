0e3a4a49fb85f4ef75a99c90b67e3e00657821d7
Liam Roche <roche.liamp@gmail.com>
2022-06-16 14:48:06 -0400

    added messiGoals.js

---
+++ messiGoals.js
@@ -0,0 +1,15 @@
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals
+
+
+console.log(goals(1,1,1))
+
+// return arguments added together
