ff754bc12ab6f044cbd64d25c86ef63d4d464280
Liam Roche <roche.liamp@gmail.com>
2022-07-22 17:17:36 -0400

    added sellStock.js

--- ransom.test.js
+++
@@ -1,6 +0,0 @@
-import test from "node:test";
-const canConstruct = require("./ransom.js")
-
-test("Tests canConstruct function from ransom.js", ()=>{
-    expect(canConstruct("aa", "ab")).toBe(false)
-})
---
+++ sellStock.js
@@ -0,0 +1,36 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+var maxProfit = function(prices) {
+    let profit = 0
+    let buy = prices[0]
+    for(let i = 1; i < prices.length; i++){
+        if(buy > prices[i]){
+            buy = prices[i]
+        }
+        let profitCheck = prices[i] - buy
+        if(profitCheck > profit){
+            profit = profitCheck 
+        }
+    }
+    return profit
+};
+
+
+console.log(maxProfit([2, 4, 1]))
+
+//define a variable to hold profit and set it to 0
+//define a variable to hold the buy price and set it as the first num in the arr
+//loop through the arr starting on the second number
+    //if buy > prices[i] 
+        //buy = prices[i]
+    
+    //define a variable to check the current price against the buy price (profitCheck)
+    //if profitCheck - buy > profit
+        //profit = profitCheck - buy
+
+//return the profit
+
