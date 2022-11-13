2d9287ad07f0cd6d272765f36e8f793111277390
Liam Roche <roche.liamp@gmail.com>
2022-11-12 20:49:31 -0500

    added reverseWords.js && pseudo-code

---
+++ reverseWords.js
@@ -0,0 +1,26 @@
+// Given an input string s, reverse the order of the words.
+
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+// Return a string of the words in reverse order concatenated by a single space.
+
+// Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+const reverseWords = (s) => {
+    const firstArr = []
+    const secodArr = s.split(' ')
+};
+
+//define an empty arr
+
+//split words into another arr at the space
+
+//loop through the second arr pushing only the words (not the spaces) into the first array
+
+//reverse the first arr
+
+//return arr.join(' ')
+
+
+
