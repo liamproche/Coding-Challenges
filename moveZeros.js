dac3f6d8758f63d8f54e12ecdde52e241438b03a
Liam Roche <roche.liamp@gmail.com>
2022-11-12 20:28:38 -0500

    solved moveZeros.js

--- moveZeros.js
+++ moveZeros.js
@@ -3,18 +3,26 @@
 // Note that you must do this in-place without making a copy of the array.
 
 var moveZeroes = function(nums) {
+    let zerosToAdd = 0
+    if(nums[0] === 0){
+        nums.splice(0, 1)
+        zerosToAdd++
+    }
     for(let i = 0; i < nums.length; i++){
         if(nums[i] === 0){
-            console.log(nums[i])
             nums.splice(i, 1)
-            nums.push(0)
+            zerosToAdd++
+            i--
         }
+    }
+    for(let i = 0; i < zerosToAdd; i++){
+        nums.push(0)
     }
     return nums
 };
 
 
-console.log(moveZeroes([0,0,1]))
+console.log(moveZeroes([0,1,0,1]))
 
 //iterate through the arr using a standard for loop
 
