bc218c5a78af21a2772f447c136cd648d2c090d4
Liam Roche <roche.liamp@gmail.com>
2022-11-21 12:20:32 -0500

    reformatted to super-slick one-liner

--- needleHaystack.js
+++ needleHaystack.js
@@ -1,13 +1,6 @@
 // Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
-const strStr = (haystack, needle) => {
-    if(!haystack.includes(needle)){
-        return -1
-    }
-    else{
-        return(haystack.indexOf(needle))
-    }
-};
+const strStr = (haystack, needle) => !haystack.includes(needle) ? -1 : haystack.indexOf(needle)
 
 console.log(strStr("leetcode", "leeto"))
 
