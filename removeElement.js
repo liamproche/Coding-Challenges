9bb21f39ff2704e374c0023b122778fe34339560
Liam Roche <roche.liamp@gmail.com>
2022-12-03 13:28:38 -0500

    added sortString.js started pseudo-code

--- removeElement.js
+++ removeElement.js
@@ -10,9 +10,10 @@ const removeElement = (nums, val) =>{
     for(let i = 0; i < nums.length; i++){
         if(nums[i] === val){
             nums.splice(i, 1)
+            i--
         }
     }
-    console.log(nums)
+    return nums
 }
 
-removeElement([3,2,2,3], 3)
+console.log(removeElement([3,2,2,3], 3))
